2d5bab5d72724192bcda983750ef74dc1de93d11
SamriddhiSanpreet <samriddhisanpreet2005@gmail.com>
2025-04-08 23:47:17 +0530

    Updates

--- Sequelize series/introduction and connecting to a database/index.js
+++ Sequelize series/introduction and connecting to a database/index.js
@@ -1,4 +1,5 @@
 const Sequelize = require('sequelize');
+const {DataTypes,Op} = Sequelize;
 const sequelize = new Sequelize('sequelize_connection','root','12345',{
     dialect:'mysql',
     // define:{
@@ -20,26 +21,26 @@ const sequelize = new Sequelize('sequelize_connection','root','12345',{
 
 const User = sequelize.define('user',{
     user_id:{
-        type: Sequelize.DataTypes.INTEGER,
+        type: DataTypes.INTEGER,
         primaryKey: true,
         autoIncrement: true,
     },
     username:{
-        type: Sequelize.DataTypes.STRING,
+        type: DataTypes.STRING,
         allowNull: false,
         validate:{
             len:[4,6] 
         }
     },
     password:{
-        type: Sequelize.DataTypes.STRING,
+        type: DataTypes.STRING,
     },
     age:{
-        type: Sequelize.DataTypes.INTEGER,
+        type: DataTypes.INTEGER,
         defaultValue:21,
     },
     sam_san:{
-        type: Sequelize.DataTypes.BOOLEAN,
+        type: DataTypes.BOOLEAN,
         defaultValue:true
     }
 },
@@ -69,57 +70,160 @@ User.sync({alter:true}).then(()=>{  // Using the force and alter is same but the
     // // console.log(user.username);
     // user.username='Ashu';
 
-    return User.bulkCreate([
-        {
-        username:'Krushi',
-        password:'0000',
-        age:19,
-        // sam_san:false
-    },{                                // it will not show any validation error because of the bulk we are using...
+//     return User.bulkCreate([
+//         {
+//         username:'Krushi',
+//         password:'0000',
+//         age:19,
+//         // sam_san:false
+//     },{                                // it will not show any validation error because of the bulk we are using...
 
-        username:'samriddhi',
-        password:'5555',
-        age:25,
-    },
-    {
-        username:'AshutoshSamriddhi'
-    }
-],
-{validate:true}) // using for bulkCreate query because as normal only the single data will show such validation as mentioned above but in the bulkCreate data this validate:true will work because it is the collection of the data.
+//         username:'samriddhi',
+//         password:'5555',
+//         age:25,
+//     },
+//     {
+//         username:'AshutoshSamriddhi'
+//     }
+// ],
+// {validate:true}) // using for bulkCreate query because as normal only the single data will show such validation as mentioned above but in the bulkCreate data this validate:true will work because it is the collection of the data.
 
-    // return User.create(
-    // {
-    //     username:'Samriddhi'
-    // })
-    .then((data)=>{
-        // console.log("User added to database successfully !!");
-        // console.log(data.toJSON());
-        // data.username = 'pizza';
-        // data.age = 20;
-        // data.decrement({age:2});
-        // data.increment({age:2,height:3});
+//     // return User.create(
+//     // {
+//     //     username:'Samriddhi'
+//     // })
+//     .then((data)=>{
+//         // console.log("User added to database successfully !!");
+//         // console.log(data.toJSON());
+//         // data.username = 'pizza';
+//         // data.age = 20;
+//         // data.decrement({age:2});
+//         // data.increment({age:2,height:3});
 
-        // return data.save();
-        // return data.destroy();
-        // return data.reload();
-        // return data.save({fields:['age']})  // only the age will save and as a result the age will be same but the previous username will be shown
-        data.forEach((element)=>{
-            console.log(element.toJSON());
-        })
+//         // return data.save();
+//         // return data.destroy();
+//         // return data.reload();
+//         // return data.save({fields:['age']})  // only the age will save and as a result the age will be same but the previous username will be shown
+//         data.forEach((element)=>{
+//             console.log(element.toJSON());
+//         })
 
-    })
-    .then((data)=>{
-        // console.log("User Updated !!");
-        // console.log(data.toJSON());
+//     })
+//     .then((data)=>{
+//         // console.log("User Updated !!");
+//         // console.log(data.toJSON());
         
 
-    });
-    // .then((data)=>{
-    //     console.log("User Updated !!");
-    //     console.log(data.toJSON());
+//     });
+//     // .then((data)=>{
+//     //     console.log("User Updated !!");
+//     //     console.log(data.toJSON());
+//     // })
+//     // user.save(); // used to save the data to the database
+//     // console.log("Table and model synced successfully !!");
+// return User.findAll(); // it shows all the data 
+// return User.findAll({attributes:['username','password']}); // it shows all username and password which i have mentioned to show in attributes.
+
+// return User.findAll({attributes:[['username','myName'],['password','pass']]}); // nested array which shows the myName intead of username and pass instead of password.
+
+// return User.findAll({attributes:[[sequelize.fn('AVG',sequelize.col('age')),'totalAge']]}); 
+
+// return User.findAll({attributes: {exclude: ['password']}}); // it used to give all data except the password field.
+
+// return User.findAll({attributes:['username'],where:{age:20,username:'Ashu'}}); // it will give the data whose age is 20 and with the name Ashu
+
+// return User.findAll({limit:2}); show only two data becauseof the given limit.
+
+// return User.findAll({order:[['age','DESC']]}); we will get the age in descending order.
+
+// return User.findAll({
+//     attributes:['username',[sequelize.fn('SUM',sequelize.col('age')),'totalAge']],   
+//     group:'username'
+// });
+
+// return User.findAll({where:{
+//     // [Op.or]:{username:'Ashu',age:20}
+//     // [Op.and]:{username:'Ashu',age:20}
+// }});
+
+// return User.findAll({where: {username:'Ashu',age:'20'}
+// });
+
+// return User.findAll({where:{
+//     // age:{
+//     //     [Op.gt]:20 // its a greater than symbol operation.
+//     // }
+//     age:{
+//         [Op.or]:{
+//             [Op.lt]:20, // less than
+//             [Op.eq]:null
+//         }
+//     }
+// }});
+
+// return User.findAll({where:
+//     sequelize.where(sequelize.fn('char_length',sequelize.col('username')), 6) // whose username characters length will be 6 they will print out.
+// });
+
+// return User.update({username:'oreo'},{
+//     where:{age:{
+//         [Op.gt]:1
+//     }}
+// });
+
+// return User.destroy({where:{username:'oreo'}});
+// return User.destroy({truncate:true});
+// return User.max('age');
+// return User.sum('age',{where:{age:21}});
+// return User.findAll({raw:true}); // give the needed data in json format
+// return User.findAll({
+//     where:{age:20},  // only age=20 data will show in json format.
+//     raw:true,
+// });
+
+// return User.findByPk(32);
+// return User.findOne(); only first one data will show
+// return User.findOne({where:{
+//     age:{
+//         [Op.or]:{
+//             [Op.lt]:25, // less than
+//             [Op.eq]:null   // equal to sign
+//         }
+//     }
+// }});
+
+// return User.findOrCreate({where:{username:'Ashu'}});
+// return User.findOrCreate({
+//     where:{
+//         username:'Jerry'
+// },
+//     defaults:{
+//             age:5
+//         }
+    
+// });
+
+return User.findAndCountAll({
+    where:{username:'lilly'},
+    raw:true
+})
+
+}).then((data)=>{
+    // data.forEach((element)=>{
+    //     console.log(element.toJSON());
     // })
-    // user.save(); // used to save the data to the database
-    // console.log("Table and model synced successfully !!");
-}).catch((err)=>{
+    // console.log(data.toJSON());
+    // console.log(data);
+    // const [result,created] = data;
+    // // console.log(result.toJSON());
+    // console.log(created);  // used to check that is data is created or not
+
+    const {rows,count} = data;
+    console.log(rows);
+    console.log(count);
+
+})
+.catch((err)=>{
     console.log("Something wrong",err);
 })
+
