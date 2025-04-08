3d591e558bb71c3fadf6d9f21895f5772ddbfb19
SamriddhiSanpreet <samriddhisanpreet2005@gmail.com>
2025-04-08 11:26:03 +0530

    Updates

--- Sequelize series/introduction and connecting to a database/index.js
+++ Sequelize series/introduction and connecting to a database/index.js
@@ -16,6 +16,7 @@ const sequelize = new Sequelize('sequelize_connection','root','12345',{
 
 // sequelize.drop({ match: /_san$/i });  // its deprecated right now so intead of this you have to use sequelize.dropTable() or force:true method.
 
+// sequelize.sync({ force: true, match: /_san$/ }); // showing an error 
 
 const User = sequelize.define('user',{
     user_id:{
@@ -25,7 +26,10 @@ const User = sequelize.define('user',{
     },
     username:{
         type: Sequelize.DataTypes.STRING,
-        allowNull: false
+        allowNull: false,
+        validate:{
+            len:[4,6] 
+        }
     },
     password:{
         type: Sequelize.DataTypes.STRING,
@@ -44,7 +48,7 @@ const User = sequelize.define('user',{
 }
 );
 
-console.log(sequelize.models.user);
+// console.log(sequelize.models.user);
 // User.drop();
 
 
@@ -59,3 +63,63 @@ console.log(sequelize.models.user);
 // }).catch((err)=>{
 //     console.log("Something wrong");
 // })
+
+User.sync({alter:true}).then(()=>{  // Using the force and alter is same but the 
+    // const user = User.build({username:'Sam',password:'123',age:'20',sam_san:true});
+    // // console.log(user.username);
+    // user.username='Ashu';
+
+    return User.bulkCreate([
+        {
+        username:'Krushi',
+        password:'0000',
+        age:19,
+        // sam_san:false
+    },{                                // it will not show any validation error because of the bulk we are using...
+
+        username:'samriddhi',
+        password:'5555',
+        age:25,
+    },
+    {
+        username:'AshutoshSamriddhi'
+    }
+],
+{validate:true}) // using for bulkCreate query because as normal only the single data will show such validation as mentioned above but in the bulkCreate data this validate:true will work because it is the collection of the data.
+
+    // return User.create(
+    // {
+    //     username:'Samriddhi'
+    // })
+    .then((data)=>{
+        // console.log("User added to database successfully !!");
+        // console.log(data.toJSON());
+        // data.username = 'pizza';
+        // data.age = 20;
+        // data.decrement({age:2});
+        // data.increment({age:2,height:3});
+
+        // return data.save();
+        // return data.destroy();
+        // return data.reload();
+        // return data.save({fields:['age']})  // only the age will save and as a result the age will be same but the previous username will be shown
+        data.forEach((element)=>{
+            console.log(element.toJSON());
+        })
+
+    })
+    .then((data)=>{
+        // console.log("User Updated !!");
+        // console.log(data.toJSON());
+        
+
+    });
+    // .then((data)=>{
+    //     console.log("User Updated !!");
+    //     console.log(data.toJSON());
+    // })
+    // user.save(); // used to save the data to the database
+    // console.log("Table and model synced successfully !!");
+}).catch((err)=>{
+    console.log("Something wrong",err);
+})
