c4a0b743e540a8b1a07826c73807404bac574c18
SamriddhiSanpreet <samriddhisanpreet2005@gmail.com>
2025-04-10 10:44:43 +0530

    Updates

--- Sequelize series/introduction and connecting to a database/index.js
+++ Sequelize series/introduction and connecting to a database/index.js
@@ -6,6 +6,11 @@ const sequelize = new Sequelize('sequelize_connection','root','12345',{
     //     freezeTableName: true,
     // }
 }) ;
+const bcrypt = require('bcrypt');
+const zlib = require('zlib');
+const { type } = require('os');
+
+
 
 // sequelize.authenticate().then(()=>{
 //     console.log("Connection Successful");
@@ -29,23 +34,98 @@ const User = sequelize.define('user',{
         type: DataTypes.STRING,
         allowNull: false,
         validate:{
-            len:[4,6] 
-        }
+            len:[4,9] 
+        },
+        // get(){
+        //     const rawValue = this.getDataValue('username');
+        //     return rawValue.toUpperCase();
+        // }
     },
     password:{
         type: DataTypes.STRING,
+        // set(value){
+        //     const salt = bcrypt.genSaltSync(12);
+        //     const hash = bcrypt.hashSync(value,salt);
+        //     this.setDataValue('password',hash);
+        // }
+        
     },
     age:{
         type: DataTypes.INTEGER,
         defaultValue:21,
+        validate:{
+            // isOldEnough(value){
+            //     if(value<21){
+            //         throw new Error("Too Young");
+            //     }
+            // }
+            // isNumeric:{
+            //     // msg:"You must enter something"
+
+            // }
+            isNumeric:true
+        }
     },
     sam_san:{
         type: DataTypes.BOOLEAN,
         defaultValue:true
+    },
+    description:{
+        type:DataTypes.STRING,
+        // set(value){
+        //   const compressed = zlib.deflateSync(value).toString('base64');
+        //   this.setDataValue('description',compressed);  
+        // },
+        // get(){
+        //     const value = this.getDataValue('description');
+        //     const uncompressed = zlib.inflateSync(Buffer.from(value,'base64')).toString();
+        //     return uncompressed;
+        //     // const uncompressed = zlib.inflateSync
+        //     // (Buffer.from(value,'base64'));
+        //     // return uncompressed;
+        // }
+    },
+    aboutUser:{
+        type:DataTypes.VIRTUAL,
+        get(){
+            return `${this.username} ${this.description}`;
+        }
+    },
+    email:{
+        type:DataTypes.STRING,
+        unique:true,
+        // validate:{
+        //     // isEmail:true
+        //     isIn:['me@sam.org','me@sam.com']
+        // }
+        allowNull:true,
+        // validate:{
+        //     isIn:{
+        //         args:['me@sam.org','me@sam.com'],
+        //         msg:'The provided email is not valid'
+        //     },
+        // }   
+        validate:{
+            myEmailValidator(value){
+                if(value === null){
+                    throw new Error("Please enter an email");
+                }
+            }
+        }
     }
 },
 {
-    freezeTableName:true //when we use this method then it means we don't want to plural our table name . suppose i haven't use this method and given my model name user then when we run and see in our mysql the the model will create but the name of the model changes to users which means it is the default behaviour of mysql to plural the table name.
+    freezeTableName:true ,//when we use this method then it means we don't want to plural our table name . suppose i haven't use this method and given my model name user then when we run and see in our mysql the the model will create but the name of the model changes to users which means it is the default behaviour of mysql to plural the table name.
+    validate:{
+        usernamePassMatch(){
+           if(this.username === this.password) {
+                throw new Error("Password can't be your username...");
+           }
+           else{
+            console.log("Khushi");
+           }
+        }
+    }
 }
 );
 
@@ -65,7 +145,12 @@ const User = sequelize.define('user',{
 //     console.log("Something wrong");
 // })
 
+function myFunction(){
+    console.log('running sql statement');
+}
+
 User.sync({alter:true}).then(()=>{  // Using the force and alter is same but the 
+
     // const user = User.build({username:'Sam',password:'123',age:'20',sam_san:true});
     // // console.log(user.username);
     // user.username='Ashu';
@@ -203,24 +288,61 @@ User.sync({alter:true}).then(()=>{  // Using the force and alter is same but the
     
 // });
 
-return User.findAndCountAll({
-    where:{username:'lilly'},
-    raw:true
-})
+// return User.findAndCountAll({
+//     where:{username:'lilly'},
+//     raw:true
+// })
+
+// return User.findOne();
+// return User.create({
+//     username:'Yashi',
+//     password:'1435',
+//     description:'This is the description its really long.'
+// })
+
+// return User.findOne({where:{username:'Yashi'}});
+// return User.create({
+//     username:'zindagi',
+//     password:'life',
+//     email:'l@gmail.com'
+// });
+// const user = User.build({email:'live'});
+// return user.validate();
+// return User.create({
+//     username:'cema',
+//     age:'5',
+//     // email:'me@sam12.org'
+//     password:'cema'
+//     // email:null
+// })
+// return sequelize.query(`UPDATE user SET age=90 WHERE username='ashu'`)
+// return sequelize.query(`select * from user`,{type:Sequelize.QueryTypes.SELECT})
+// return sequelize.query(`update user set age = 20 where username ='ashu'`,{type:Sequelize.QueryTypes.UPDATE});
+// return sequelize.query(`select * from user limit 2`,{model:User,plain:true});
+
+return sequelize.query(`select * from user limit 2`,{logging:myFunction})
 
 }).then((data)=>{
     // data.forEach((element)=>{
     //     console.log(element.toJSON());
     // })
     // console.log(data.toJSON());
-    // console.log(data);
+    console.log(data);
     // const [result,created] = data;
     // // console.log(result.toJSON());
     // console.log(created);  // used to check that is data is created or not
 
-    const {rows,count} = data;
-    console.log(rows);
-    console.log(count);
+    // const {rows,count} = data;
+    // console.log(rows);
+    // console.log(count);
+
+    // console.log(data.username);
+    // console.log(data.password);
+    // console.log(data.description);
+    // console.log(data.aboutUser);
+    // [result,metadata]=data;
+    // console.log(result);
+    // console.log(metadata);
 
 })
 .catch((err)=>{
